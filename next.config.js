8b71f6c6cba4cc6337f55b935cfb7a63e655e671
m2vi <iuvo@gmx.at>
2021-04-01 00:24:34 +0200

    .

--- next.config.js
+++ next.config.js
@@ -13,19 +13,3 @@ module.exports = withReactSvg({
     return config;
   },
 });
-
-module.exports = {
-  env: {},
-};
-
-// const withTM = require('next-transpile-modules');
-// const withPlugins = require('next-compose-plugins');
-
-// module.exports = withPlugins([
-//   [
-//     withTM,
-//     {
-//       transpileModules: ['react-syntax-highlighter'],
-//     },
-//   ],
-// ]);
